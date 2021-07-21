fd8cc77ebdce76d92c58fdc01fe2bc89df1e1996
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-07-21 09:04:38 -0300

    Configurando o Swagger

--- cdc/src/main/java/the/coyote/cdc/Controllers/ControllerTest.java
+++ cdc/src/main/java/the/coyote/cdc/Controllers/ControllerTest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 public class ControllerTest {
 	
-	@GetMapping(value = "/")
+	@GetMapping(value = "/api")
 	public String test() {
 		return "Api configurada";
 	}
